9b980a08d39874ae6455c1ae1ff9a90aefad8084
elizaveta-rybina <liza.rybina@inbox.ru>
2025-01-17 18:14:08 +0300

    #feat: добавлен роутинг

---
+++ src/app/routes/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+const router = createHashRouter([
+	{
+		path: '/',
+		//element: <HomePage />,
+	},
+	{
+		path: '/favorites',
+		//element: <FavoritesPage />,
+	},
+])
+
+export const AppRouter = () => {
+	return <RouterProvider router={router} />
+}
