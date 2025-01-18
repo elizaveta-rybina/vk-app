a5f7620183fedcafacf5052368c1aabf60ab6a4d
elizaveta-rybina <liza.rybina@inbox.ru>
2025-01-19 01:49:44 +0300

    #fix: убрано хэширование в url

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,8 +1,8 @@
 import { FavoritesPage, HomePage } from 'pages'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Container from 'shared/Container'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Container />,
