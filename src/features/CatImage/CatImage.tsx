d63ad0b03b5a95db5c31dccd7b8df40bc9582c16
elizavetarybina <liza.rybina@inbox.ru>
2025-01-21 21:49:38 +0300

    #feat: Вынесение лишней логики из компонента

--- src/features/CatImage/CatImage.tsx
+++ src/features/CatImage/CatImage.tsx
@@ -1,10 +1,12 @@
 import { Cat } from 'app/stores/types'
+import HeartIcon from 'features/HeartIcon'
 import styles from './CatImage.module.scss'
 
 const CatImage = ({ cat }: { cat: Cat }) => {
   return (
     <div className={styles.catImage}>
       <img src={cat.url} alt={`Cat ${cat.id}`} className={styles.catImg} />
+      <HeartIcon cat={cat} />
     </div>
   );
 };
