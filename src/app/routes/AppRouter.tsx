f03292f3d0da95d75a20c3aa777fcc6f6d85ad21
elizavetarybina <liza.rybina@inbox.ru>
2025-01-21 22:57:15 +0300

    #build: изменения для github pages

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -2,22 +2,27 @@ import { FavoritesPage, HomePage } from 'pages'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Container from 'shared/Container'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Container />,
+			children: [
+				{
+					path: '/',
+					element: <HomePage />,
+				},
+				{
+					path: '/favorites',
+					element: <FavoritesPage />,
+				},
+			],
+		},
+	],
 	{
-		path: '/',
-		element: <Container />,
-		children: [
-			{
-				path: '/',
-				element: <HomePage />,
-			},
-			{
-				path: '/favorites',
-				element: <FavoritesPage />,
-			},
-		],
-	},
-])
+		basename: '/vk-app',
+	}
+)
 
 export const AppRouter = () => {
 	return <RouterProvider router={router} />
