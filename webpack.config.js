d33301024657c677343a73ba568ef4d573550162
elizavetarybina <liza.rybina@inbox.ru>
2026-03-10 20:17:42 +0300

    #fix: dependency vulnerabilities fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,49 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-  mode: 'development',
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: './src/index.tsx',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      app: path.resolve(__dirname, 'src/app'),
+      entities: path.resolve(__dirname, 'src/entities'),
+      features: path.resolve(__dirname, 'src/features'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      shared: path.resolve(__dirname, 'src/shared'),
+      widgets: path.resolve(__dirname, 'src/widgets'),
+    },
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.module\.scss$/,
@@ -39,4 +67,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
